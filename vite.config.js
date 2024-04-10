b2a93e8ae1ea67300d6296c1e9723447933cb1b2
Kanish Mohariya <kanishmohariya1@gmail.com>
2024-04-10 21:41:04 +0530

    added backend link

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://aeonaxy-task-backend-nm0w.onrender.com/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
